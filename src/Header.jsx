7fbc158397836cbf9ba04dca678d6da42b3d567c
Ashikul Islam <ashikulislamfb@gmail.com>
2025-07-07 17:29:47 +0600

    refined

--- src/Header.jsx
+++ src/Header.jsx
@@ -22,8 +22,8 @@ export default function Header() {
                     <CartDetails onCancel={() => setShowCart(false)} />
                 )}
                 <nav className="container flex items-center justify-between space-x-10 py-6">
-                    <a href="index.html">
-                        <img src={logo} width="139" height="26" alt="logo" />
+                    <a href="index.html" className="text-2xl text-white font-serif font-bold">
+                        MOVIE SHOP
                     </a>
 
                     <ul className="flex items-center space-x-5">
